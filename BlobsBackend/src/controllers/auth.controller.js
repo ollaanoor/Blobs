7478bdb36928ab2bd8ba3c59bb84576a8c55035a
ollaanoor <ollaanoor65@gmail.com>
2025-05-02 02:30:15 +0300

    fix cookie

--- BlobsBackend/src/controllers/auth.controller.js
+++ BlobsBackend/src/controllers/auth.controller.js
@@ -51,7 +51,7 @@ exports.login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "none",
+      sameSite: "None",
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     });
 
