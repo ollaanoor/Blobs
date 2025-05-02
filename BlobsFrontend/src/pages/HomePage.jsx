72a97b0d1d1e4cc2f2e931d30b0a9dadd41a0ef2
ollaanoor <ollaanoor65@gmail.com>
2025-05-02 03:42:45 +0300

    create post test

--- BlobsFrontend/src/pages/HomePage.jsx
+++ BlobsFrontend/src/pages/HomePage.jsx
@@ -48,6 +48,7 @@ export default function HomePage() {
     } catch (error) {
       console.log("Error creating post", error);
     }
+    cobsole.log(posts);
   };
 
   const handleDeletePost = async (postId) => {
